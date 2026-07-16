c8bbebf1453ecd70e798e11f11334cc9725555e8
renadali76 <r3n66d@icloud.com>
2026-07-16 22:23:24 +0300

    adding routes

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
