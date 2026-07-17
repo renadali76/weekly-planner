478e0314b2bddc0d02b0b9eb81a535e00bde4c53
renadali76 <r3n66d@icloud.com>
2026-07-17 16:11:47 +0300

    Implementing POST method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,13 +10,25 @@ def home():
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register" , methods = ["GET" , "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print(username)
+        print(email)
+        print(password)
+        return "Registration successful!"
+
+
     return render_template("register.html")
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
